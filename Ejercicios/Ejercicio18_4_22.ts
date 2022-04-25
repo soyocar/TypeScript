80e250f05050f5e610eb9e603150860d29e73467
soyocar <royomek@gmail.com>
2022-04-25 16:10:25 -0300

    commit

---
+++ Ejercicios/Ejercicio18_4_22.ts
@@ -0,0 +1,12 @@
+// Pasos para hacer un buen mate.
+console.log("Si la pava no tiene agua agregar hasta llenarlo");
+console.log("Encender la hornalla de la cocina y poner la pava a calentar");
+console.log("Verificar la temperatura del agua aproximadamente a 75 Grados");
+console.log("Poner yerba en el mate hasta un 80 %");
+console.log(
+  "inclinar el mate unos 45 grados para ubicar la yerba hacia un costado"
+);
+console.log("Poner en un vaso agua tibia y verterlo sobre el mate");
+console.log("Poner la bombilla");
+console.log("Verter el agua de la pava en el costado del mate con menos yerba");
+console.log("Deleitar unos buenos mates!!");
